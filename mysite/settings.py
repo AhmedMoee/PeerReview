0900d2c6bc023ca96a29769e47869788e16b5f09
Ahmed <ahmedm@virginia.edu>
2024-09-23 20:39:11 -0400

    Attempting to sync SITE_ID between dev and prod

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'uva-cs-3240-project-b-10-7d152fd5a13
 # Application definition
 
 # SITE_ID=4 # local
-SITE_ID=5 # prod
+SITE_ID=4 # prod
 
 INSTALLED_APPS = [
     'django.contrib.admin',

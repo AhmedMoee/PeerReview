59bbb82de41bc9f5b597b75074f4ebc6bfa17e50
Ahmed <ahmedm@virginia.edu>
2024-10-16 20:21:10 -0400

    database postgres change

--- mysite/settings.py
+++ mysite/settings.py
@@ -94,18 +94,18 @@ ASGI_APPLICATION = 'mysite.asgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # 'default': dj_database_url.config(
-    #     default=os.getenv('DATABASE_URL', 'postgres://{}:{}@{}:{}/{}'.format(
-    #         os.getenv('DB_USER'),
-    #         os.getenv('DB_PASSWORD'),
-    #         os.getenv('DB_HOST'),
-    #         os.getenv('DB_PORT'),
-    #         os.getenv('DB_NAME')
-    #     )))
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'postgres://{}:{}@{}:{}/{}'.format(
+            os.getenv('DB_USER'),
+            os.getenv('DB_PASSWORD'),
+            os.getenv('DB_HOST'),
+            os.getenv('DB_PORT'),
+            os.getenv('DB_NAME')
+        )))
 }
 
 
